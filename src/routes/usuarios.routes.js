0bc74c0953182fccfcdbe9902c5d69749ae832f5
Felipe Santos <dev.felipesantos@gmail.com>
2024-10-31 11:19:44 -0300

    feat: create update user method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,6 +3,7 @@ import UsersRepository from "../models/users/UsersRepository.js";
 
 const usuariosRoutes = Router();
 
+// Criação da instância do repositório de usuários
 const usersRepository = new UsersRepository();
 
 usuariosRoutes.get("/", (req, res) => {
@@ -19,6 +20,9 @@ usuariosRoutes.get("/", (req, res) => {
 
 usuariosRoutes.post("/", (req, res) => {
   const { name, email, password } = req.body;
+  // const name = req.body.name
+  // const email = req.body.email
+  // const password = req.body.password
 
   const usuario = usersRepository.addUser(name, email, password);
 
@@ -27,5 +31,41 @@ usuariosRoutes.post("/", (req, res) => {
     usuario,
   });
 });
+
+usuariosRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
+  // const id = req.params.id
+  const user = usersRepository.getUserById(id);
+
+  if (!user) {
+    return res.status(404).json({
+      message: `Usuário com id ${id} não encontrado!`,
+    });
+  }
+
+  return res.status(200).json({
+    message: `Usuário com id ${id} encontrado!`,
+    user,
+  });
+});
+
+usuariosRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+
+  const user = usersRepository.updateUser(id, name, email, password);
+
+  if (!user) {
+    return res.status(404).json({
+      message: `Usuário com id ${id} não encontrado!`,
+    });
+  }
+
+  return res.status(200).json({
+    message: `Usuário com id ${id} atualizado com sucesso!`,
+    user,
+  });
+});
+usuariosRoutes.delete("/:id", (req, res) => {});
 
 export default usuariosRoutes;
