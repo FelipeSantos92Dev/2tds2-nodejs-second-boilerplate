3dc706f146a0e5dafd5a08d214d7c70ca842d5ba
Felipe Santos <dev.felipesantos@gmail.com>
2024-10-31 09:48:15 -0300

    feat: create add user method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,16 +1,30 @@
 import { Router } from "express";
+import UsersRepository from "../models/users/UsersRepository.js";
 
 const usuariosRoutes = Router();
 
-let usuarios = [];
+const usersRepository = new UsersRepository();
 
 usuariosRoutes.get("/", (req, res) => {
+  const usuarios = usersRepository.getAllUsers();
+
   return res.status(200).json({
     message:
       usuarios.length == 0
         ? "Não há usuários cadastrados"
         : `Total de usuários: ${usuarios.length}`,
     usuarios,
+  });
+});
+
+usuariosRoutes.post("/", (req, res) => {
+  const { name, email, password } = req.body;
+
+  const usuario = usersRepository.addUser(name, email, password);
+
+  return res.status(201).json({
+    message: "Usuário cadastrado com sucesso!",
+    usuario,
   });
 });
 
